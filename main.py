f4d6809a75508feb5726c6636d8c24599b81e33f
Ampercloud <122858420+Ampercloud@users.noreply.github.com>
2023-07-31 16:34:30 +0800

    Add files via upload

---
+++ main.py
@@ -0,0 +1,14 @@
+# This is a simple Streamlit script.
+import streamlit as st
+
+
+def main():
+
+    st.write('Hello world!')
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    main()
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
