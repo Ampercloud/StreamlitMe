52507c8c899d8c25808286bf2bd29a33d638d13c
Ampercloud <122858420+Ampercloud@users.noreply.github.com>
2023-07-31 21:31:12 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,42 +1,42 @@
 # This is a simple Streamlit script.
 import streamlit as st
 import pandas as pd
+import numpy as np
 
 
 def main():
-    st.markdown('# Avocado Prices dashboard')
+    st.title('Uber pickups in NYC')
 
-    # # Set the app title
-    # st.title('xG Match Result Prediction App')
-    # # Add a text box to the app
-    # text = st.text(
-    #     'Using known xG figures for a match, enter them into the boxes below.\n'
-    #     'First prediction is made from an XGBoost Regressor trained on a dataset from\n'
-    #     'understat.\n'
-    #     'Second prediction uses a ZIF Poisson model with a theta of 0.08 to account\n'
-    #     'for the 0-0.'
-    # )
-    #
-    # # Add input fields for home and away xG
-    # home_xg = st.number_input('Home xG', min_value=0.0, max_value=6.0, step=0.06, format="%.6f")
-    # away_xg = st.number_input('Away xG', min_value=0.0, max_value=6.0, step=0.06, format="%.6f")
-    # # Add a button to trigger the prediction
-    # if st.button('Predict'):
-    #     if home_xg == 0.0 or away_xg == 0.0:
-    #         st.error('Please enter an xG value for both teams.')
-    #     else:
-    #         # Preprocess the input data
-    #         input_data = preprocess_data(home_xg, away_xg)
-    #         # Make the prediction
-    #         prediction = xgb_model.predict(input_data)[0]
-    #         # Normalize the prediction
-    #         prediction_normalized = prediction / sum(prediction)
-    #         st.success("The predicted match outcomes are:\nHome team: {:.4f}\nDraw: {:.4f}\nAway team: {:.4f}".format(
-    #             prediction_normalized[0], prediction_normalized[1], prediction_normalized[2]))
-    #         st.success("The summed match outcomes are: {:.4f}".format(sum(prediction_normalized)))
-    #
-    #     # Make the Poisson model prediction
-    #     poisson_prediction = zero_inflated_poisson_model(home_xg, away_xg)
+    DATE_COLUMN = 'date/time'
+    DATA_URL = ('https://s3-us-west-2.amazonaws.com/'
+                'streamlit-demo-data/uber-raw-data-sep14.csv.gz')
+
+    @st.cache_data
+    def load_data(nrows):
+        data = pd.read_csv(DATA_URL, nrows=nrows)
+        lowercase = lambda x: str(x).lower()
+        data.rename(lowercase, axis='columns', inplace=True)
+        data[DATE_COLUMN] = pd.to_datetime(data[DATE_COLUMN])
+        return data
+
+    data_load_state = st.text('Loading data...')
+    data = load_data(10000)
+    data_load_state.text("Done! (using st.cache_data)")
+
+    if st.checkbox('Show raw data'):
+        st.subheader('Raw data')
+        st.write(data)
+
+    st.subheader('Number of pickups by hour')
+    hist_values = np.histogram(data[DATE_COLUMN].dt.hour, bins=24, range=(0, 24))[0]
+    st.bar_chart(hist_values)
+
+    # Some number in the range 0-23
+    hour_to_filter = st.slider('hour', 0, 23, 17)
+    filtered_data = data[data[DATE_COLUMN].dt.hour == hour_to_filter]
+
+    st.subheader('Map of all pickups at %s:00' % hour_to_filter)
+    st.map(filtered_data)
 
 
 # Press the green button in the gutter to run the script.
